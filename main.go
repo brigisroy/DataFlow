948d093bf9453602c2dbbb31af33946045af98ac
Brigis J <brigisroy@gmail.com>
2025-01-11 13:43:52 +0530

    updated to sync csv file with go routines to sync every 30 minutes

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"time"
 
 	"sample.code/dataflow/src/adapters/repository/postgres"
 	"sample.code/dataflow/src/core/config"
@@ -17,5 +20,24 @@ func main() {
 	syncDataRepo := postgres.NewSyncDataRepository(dbPool)
 	syncDataService := service.NewSyncDataService(config.App.DataFilePath, syncDataRepo)
 
-	syncDataService.SyncData()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ticketChannel := make(chan bool, 1)
+	ticker := time.NewTicker(30 * time.Minute)
+	defer ticker.Stop()
+	go func() {
+		// Wait for the ticker to expire and signal the syncData method
+		<-ticker.C
+		// Ticket for 30 minutes elapsed, trigger sync data
+		ticketChannel <- true
+	}()
+
+	// Wait for the ticket signal and call SyncData
+	select {
+	case <-ticketChannel:
+		// After 30 minutes, trigger the sync data
+		fmt.Println("30 minutes elapsed. Syncing data...")
+		syncDataService.SyncData(ctx) // Trigger your sync process here
+	}
 }
