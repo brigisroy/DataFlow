610a12da952055e89a35ee93c7f0be9c8ad8d419
Brigis J <brigisroy@gmail.com>
2025-01-11 16:00:36 +0530

    update in main.go. removed the commented code.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"sample.code/dataflow/src/adapters/handler"
@@ -25,26 +27,26 @@ func main() {
 
 	revenueHnadler := handler.NewRevenueHandler(revenueService)
 
-	//ctx, cancel := context.WithCancel(context.Background())
-	//defer cancel()
-	//
-	//ticketChannel := make(chan bool, 1)
-	//ticker := time.NewTicker(30 * time.Minute)
-	//defer ticker.Stop()
-	//go func() {
-	//	// Wait for the ticker to expire and signal the syncData method
-	//	<-ticker.C
-	//	// Ticket for 30 minutes elapsed, trigger sync data
-	//	ticketChannel <- true
-	//}()
-	//
-	//// Wait for the ticket signal and call SyncData
-	//select {
-	//case <-ticketChannel:
-	//	//After 30 minutes, trigger the sync data
-	//	fmt.Println("30 minutes elapsed. Syncing data...")
-	//	syncDataService.SyncData(ctx) // Trigger your sync process here
-	//}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ticketChannel := make(chan bool, 1)
+	ticker := time.NewTicker(30 * time.Minute)
+	defer ticker.Stop()
+	go func() {
+		// Wait for the ticker to expire and signal the syncData method
+		<-ticker.C
+		// Ticket for 30 minutes elapsed, trigger sync data
+		ticketChannel <- true
+	}()
+
+	// Wait for the ticket signal and call SyncData
+	select {
+	case <-ticketChannel:
+		//After 30 minutes, trigger the sync data
+		fmt.Println("30 minutes elapsed. Syncing data...")
+		syncDataService.SyncData(ctx) // Trigger your sync process here
+	}
 	_ = syncDataService
 	e := echo.New()
 
